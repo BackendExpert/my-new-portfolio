9f771c02d172adb86f08c278370097a7f5441e51
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-29 20:42:43 +0530

    Update

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import SideArea from './SideArea'
 import MainContent from './MainContent'
+import './style.css'
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen site-bg px-4 md:px-40 text-white py-8'>
-        <div className="flex">
-            <div className="w-full md:w-1/4">
-                <SideArea />
-            </div>
-            <div className="w-full">
-                <MainContent />
-            </div>
-        </div>
+<div className=' site-bg px-4 md:px-40 text-white py-8'>
+  <div className="md:flex">
+    <div className="w-full md:w-1/4">
+      <SideArea />
     </div>
+    <div className="w-full md:ml-4 my-4 md:my-0">
+      <div className="overflow-y-auto max-h-screen scrollbar-hide">
+        <MainContent />
+      </div>
+    </div>
+  </div>
+</div>
+
+
   )
 }
 
