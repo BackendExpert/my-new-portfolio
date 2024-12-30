19d42b94333aea0264499fbd586c1fddd281c94a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-30 19:25:26 +0530

    UPdate

---
+++ src/pages/Home/Education.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='my-4'>
+        <h1 className="text-2xl font-semibold text-[#ffc107]">Education</h1>
+    </div>
+  )
+}
+
+export default Education
--- src/pages/Home/MainContent.jsx
+++ src/pages/Home/MainContent.jsx
@@ -6,44 +6,45 @@ import AboutMe from './AboutMe';
 import SkillData from './SkillData';
 import Projects from './Projects';
 import Experience from './Experience';
+import Education from './Education';
 
 const MainContent = () => {
 
 
   return (
     <div className="">
-        <div className="relative">
-            <div className="bg-[url(https://wallpapercave.com/wp/wp8437461.jpg)] p-4 h-96 bg-center bg-cover rounded-md">
-                <div className="absolute inset-0 bg-gradient-to-t from-[#191923] to-[#191923]/50 opacity-90 z-10"></div>
-                <div className="relative z-20 text-white mt-32">
-                    <h1 className="text-2xl md:text-5xl leading-tight italic">Discover my Amazing <br /> Projects</h1>
-                    <p className="text-sm text-[#ffc107] mt-4">
-                        <TypeAnimation
-                            sequence={[
-                                'I Build MERN Stack + MVC App',
-                                1000, 
-                                'I Build Desktop applications using ElectronJS',
-                                1000,
-                                'I Build Mobile App using React Native + Expo',
-                                1000,
-                                'I Build NPM Packages',
-                                1000,
-                                'I Build Visual Studio Code Extensions',
-                                1000
-                            ]}
-                            wrapper="span"
-                            speed={50}
-                            style={{ fontSize: '1em', display: 'inline-block' }}
-                            repeat={Infinity}
-                        />
-                    </p>
+            <div className="relative">
+                <div className="bg-[url(https://wallpapercave.com/wp/wp8437461.jpg)] p-4 h-96 bg-center bg-cover rounded-md">
+                    <div className="absolute inset-0 bg-gradient-to-t from-[#191923] to-[#191923]/50 opacity-90 z-10"></div>
+                    <div className="relative z-20 text-white mt-32">
+                        <h1 className="text-2xl md:text-5xl leading-tight italic">Discover my Amazing <br /> Projects</h1>
+                        <p className="text-sm text-[#ffc107] mt-4">
+                            <TypeAnimation
+                                sequence={[
+                                    'I Build MERN Stack + MVC App',
+                                    1000, 
+                                    'I Build Desktop applications using ElectronJS',
+                                    1000,
+                                    'I Build Mobile App using React Native + Expo',
+                                    1000,
+                                    'I Build NPM Packages',
+                                    1000,
+                                    'I Build Visual Studio Code Extensions',
+                                    1000
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ fontSize: '1em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
+                        </p>
 
-                    <div className="mt-4">
-                        <Button btnvale={'Explore More'}/>
+                        <div className="mt-4">
+                            <Button btnvale={'Explore More'}/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         <div className="">
             <CardData />
         </div>
@@ -54,11 +55,15 @@ const MainContent = () => {
             <SkillData />
         </div>
         <div className="">
+            <Education />
+        </div>
+        <div className="">
             <Experience />
         </div>
         <div className="">
             <Projects />
         </div>
+        
     </div>
 
 
