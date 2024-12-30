94fbd0a6cd02fd2570048359ab798467754b8853
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-30 19:52:41 +0530

    Update Education.jsx

--- src/pages/Home/Education.jsx
+++ src/pages/Home/Education.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
 const Education = () => {
+    const education = [
+        {
+            id: 1,
+            name: `St Sylvester's College`,
+            qulification: 'Rrimary and Secondary Education',
+            date: '2006 - 2020',
+            desc: 'Advanced Level in Technology',
+            img: 'https://upload.wikimedia.org/wikipedia/si/thumb/d/dc/St._Sylvester%27s_College.png/120px-St._Sylvester%27s_College.png'
+        },
+        {
+            id: 2,
+            name: 'Sri Lanka International Buddhist Academy (SIBA Campus)',
+            qulification: 'BSc in Information Technology',
+            date: 'Oct-2021 to Auguest 2024',
+            desc: 'The education institute of Sri Dalada Maligawa',
+            img: 'https://media.licdn.com/dms/image/v2/C4E0BAQFlyf-lGV8CwA/company-logo_200_200/company-logo_200_200/0/1631304536997?e=1743638400&v=beta&t=awDzpVekufqb1jo9OEHDBaG6gaIoQ3Y9xsT9y18vUF4'
+        },
+        {
+            id: 3,
+            name: 'Sri Lanka Institute of Tourism & Hotel Management',
+            qulification: 'Foundation in Pastry and Bakery',
+            date: 'Oct-2023 to Dec-2023',
+            desc: 'Foundation in Pastry and Bakery',
+            img: 'https://media.licdn.com/dms/image/v2/C4E0BAQF5coLdyzBb1g/company-logo_100_100/company-logo_100_100/0/1630648837019?e=1743638400&v=beta&t=YPP13l24R1QEfcuPcZUJ96t-Aak5rUaec-J3MKjteNk'
+        },
+        {
+            id: 4,
+            name: 'CCBS Campus',
+            qulification: 'Varieties of Pizza',
+            date: 'Dec 2024',
+            desc: 'Varieties of Pizza',
+            img: 'https://media.licdn.com/dms/image/v2/D560BAQH0wRlA73uF3A/company-logo_100_100/company-logo_100_100/0/1693291268306?e=1743638400&v=beta&t=MWwW29Mu8571wcbNvgivrREmOPYE0iIXOYi7fNB2IPM'
+        },
+    ]
   return (
     <div className='my-4'>
         <h1 className="text-2xl font-semibold text-[#ffc107]">Education</h1>
+        <div className="">
+            <div className="md:grid grid-cols-3 gap-3 my-4">
+                {
+                    education.map((item, index) => {
+                        return (
+                            <div className="" key={index}>
+                                
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     </div>
   )
 }
