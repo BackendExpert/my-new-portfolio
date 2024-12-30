e9c09fc9af56607647618b25a1fe1a9e80fe5de1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-30 17:14:18 +0530

    Update

--- src/pages/Home/Experience.jsx
+++ src/pages/Home/Experience.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import NoImg from '../../assets/ImageNo.png'
 
 const Experience = () => {
+    const job = [
+      {
+        id: 1,
+        post: 'Founder and Developer',
+        name: 'JKCSS - CSS Framework',
+        date: 'May 2023 - Present',
+        img: 'https://avatars.githubusercontent.com/u/139304882?s=200&v=4',
+        desc: 'Created and maintain South Asia’s first CSS framework, now in its 6th major public release. Leading the development of South Asian first Git platform, expected to take 3–4 years for its first public release. Managing overall development, vision, and team coordination to ensure innovative and efficient product design.'
+      },
+      {
+        id: 2,
+        post: 'Founder and Developer',
+        name: 'CoconutDB',
+        date: 'November 2024 - Present',
+        img: 'https://avatars.githubusercontent.com/u/189954645?s=200&v=4',
+        desc: 'Developing Coconut DB, a NoSQL document-oriented database designed for flexible and scalable data storage. The database aims to provide a lightweight solution for managing structured and semi-structured data with ease.'
+      },
+      {
+        id: 3,
+        post: 'Owner and CEO',
+        name: 'JehanKandy Photography',
+        date: 'September 2024 - Present',
+        img: {NoImg},
+        desc: 'Specializing in portrait photography using a Sony A6400.'
+      },
+      {
+        id: 4,
+        post: 'Volunteer Research Assistant and Software Development Intern',
+        name: 'National Institute of Fundamental Studies',
+        date: 'January 2024 - October 2024',
+        img: 'https://media.licdn.com/dms/image/v2/C510BAQErFzbLAk27gg/company-logo_100_100/company-logo_100_100/0/1630595150759/nifslk_logo?e=1743638400&v=beta&t=3Ws_0gWgIel24A2z4dwtV7dWFLlogxC0lT-Dms3jPsg',
+        desc: 'Specializing in portrait photography using a Sony A6400.'
+      }
+    ]
   return (
     <div className='my-4'>
         <h1 className="text-2xl font-semibold text-[#ffc107]">Experience</h1>
+
+        <div className=""></div>
     </div>
   )
 }
