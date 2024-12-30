71ed35df1b01c6b8152238b7852202b974a22244
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-30 17:55:19 +0530

    Update Projects.jsx

--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 
 const Projects = () => {
+    const  project = [
+      {
+        id: 1,
+        name: 'CoconetDB Windows Desktop Application',
+        projectDate: 'Dec 2024 - Present',
+        image: 'https://avatars.githubusercontent.com/u/189954645?s=200&v=4',
+        desc: 'Developed the CoconetDB Windows Desktop Application, a NoSQL document-oriented database management tool. CoconetDB is the first NoSQL database in South Asia',
+        moreinfo: '',
+        linkedProject: 'https://www.linkedin.com/in/jehanweerasuriya/details/projects/'
+      },
+      {
+        id: 2,
+        name: 'CoconutDB Web Interface',
+        projectDate: 'Dec 2024 - Present',
+        image: 'https://avatars.githubusercontent.com/u/189954645?s=200&v=4',
+        desc: 'Developed an innovative web interface for CoconutDB, a NoSQL document-oriented database program that simplifies database management and enhances developer productivity.',
+        moreinfo: '',
+        linkedProject: 'https://www.linkedin.com/in/jehanweerasuriya/details/projects/'
+      },
+      {
+        id: 3,
+        name: 'TestAPIze - API Development and Testing Tool',
+        projectDate: 'Dec 2024 - Present',
+        image: '',
+        desc: 'TestAPIze is an API testing and development tool designed to streamline the API workflow for developers, similar to Postman. ',
+        moreinfo: '',
+        linkedProject: 'https://www.linkedin.com/in/jehanweerasuriya/details/projects/'
+      },
+      {
+        id: 4,
+        name: 'NoSQL Document-Oriented Database Program (In Development) - CoconutDB',
+        projectDate: 'Nov 2024 - Present',
+        image: 'https://avatars.githubusercontent.com/u/189954645?s=200&v=4',
+        desc: 'Currently in its early stages of development, this NoSQL document-oriented database program is designed to provide a flexible and scalable solution for managing structured and semi-structured data.',
+        moreinfo: '',
+        linkedProject: 'https://www.linkedin.com/in/jehanweerasuriya/details/projects/'
+      },
+      {
+        id: 5,
+        name: 'Setup-Project-MERN-with-MVC – Rapid MERN Stack Project Initialization',
+        projectDate: 'Sep 2024 - Present',
+        image: 'https://img.icons8.com/?size=80&id=1VuwsQVEpfkq&format=png',
+        desc: 'Setup-Project-MERN-with-MVC is an innovative NPM package that allows full-stack developers to create both front-end and back-end environments for MERN stack applications in under 1 minute using a single command.',
+        moreinfo: '',
+        linkedProject: 'https://www.linkedin.com/in/jehanweerasuriya/details/projects/'
+      },
+    ]
   return (
     <div className='my-4'>
         <h1 className="text-2xl font-semibold text-[#ffc107]">Notable Projects</h1>
+        <div className="">
+
+        </div>
     </div>
     
   )
